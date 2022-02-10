d7c1594e9b94b275dba3d8a2455e84327be5a008
Aleksandra Kostina <drakon@MacBook-Pro-de-Aleksandra.local>
2022-02-10 15:49:05 +0100

    #1310 Composant sideral affiche les donnees ecrites en dur

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -9,14 +9,20 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  users=[];
+  //users=[];
   constructor(private userService: UsersService) { }
 
   ngOnInit(): void {
-    console.log(this.userService.getUsers());
+    //console.log(this.userService.getUsers());
      
   }
 
+  first_name: string = 'Sasha';
+  last_name: string = 'Kostina';
+  birthdate: string = '1988-02-05';
+  city: string = 'Toulouse';
+  availability: string = "Immediatement";
+  
   
 
 }
