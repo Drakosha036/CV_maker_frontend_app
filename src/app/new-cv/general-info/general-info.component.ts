81fc6f060486d0762b051bea5780326cccb82e0f
Aleksandra <sashakostina@yahoo.com>
2022-03-01 13:26:49 +0100

    #1321 Remplir la page d'info generale avec le formulaire, checkboxes, photo et les boutons. Reformatter le css

--- src/app/new-cv/general-info/general-info.component.ts
+++ src/app/new-cv/general-info/general-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-general-info',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralInfoComponent implements OnInit {
 
+
+  submitted = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //formulaire
+  profileForm = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    birthday: new FormControl(''),
+    city: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  onSubmit() {
+    this.submitted = true;
   }
 
 }
