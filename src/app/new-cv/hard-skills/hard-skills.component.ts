31d3a5b30cc7e30b8edb66977c96f4d4b2de4f01
Aleksandra <sashakostina@yahoo.com>
2022-03-01 22:09:03 +0100

    1712 Mettre en place la page hard skills. A retravailler

--- src/app/new-cv/hard-skills/hard-skills.component.ts
+++ src/app/new-cv/hard-skills/hard-skills.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HardSkillsListComponent } from 'src/app/admin/hard-skills-list/hard-skills-list.component';
 
 @Component({
   selector: 'app-hard-skills',
@@ -11,5 +13,24 @@ export class HardSkillsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  hardskillList = [{hardskillText : 'someHardSkill', done : false}];
+/*
+  onSubmit() {
+    //this.hardskills = ;
+    console.log(this.hardskillList);
+  }
+  */
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+
+  hardskillInput = "";
+  hardskillAdd() {
+    this.hardskillList.push({hardskillText : this.hardskillInput, done: false});
+  } 
+  
+
 
 }
